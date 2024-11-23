e9fd905ac065d273c659808e1c7f7fff217921bb
anshika-maurya <anshikamaurya6400@gmail.com>
2024-11-23 20:15:15 +0530

    initial

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -91,7 +91,7 @@ const Catalog = () => {
                 } cursor-pointer`}
                 onClick={() => setActive(1)}
               >
-                Most Populer
+                Most Popular
               </p>
               <p
                 className={`px-4 py-2 ${
